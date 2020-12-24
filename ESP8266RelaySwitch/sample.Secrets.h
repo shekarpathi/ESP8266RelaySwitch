6acb6ed842f622fef6a34161973f2a0f1ddc1369
Shekar <shekar.lakshmipathi+bitbucket@gmail.com>
2020-12-24 13:58:29 -0500

    Changes

--- ESP8266RelaySwitch/sample.Secrets.h
+++ ESP8266RelaySwitch/sample.Secrets.h
@@ -1,15 +1,9 @@
 #ifndef SECRETS_H
 #define SECRETS_H
-  String espname="FoyerSwitch";
+  String espname="FoyerESP";
   String switchpassword="MySecretPassword";
-  const char * WiFiSID1="SID1";
-  const char * WiFiPWD1="RobinHood1";
-  const char * WiFiSID2="SID2";
-  const char * WiFiPWD2="RobinHood2";
-  const char * WiFiSID3="SID3";
-  const char * WiFiPWD3="RobinHood3";
-  const char * WiFiSID4="SID4";
-  const char * WiFiPWD4="RobinHood4";
+  char * WiFiSID1="SID1";
+  char * WiFiPWD1="RobinHood1";
   char * emailSendTo="test@test.com"
   const char* EMAIL_FROM="foo.bar@hotmail.com";
   const char* EMAIL_PASSWORD="thisisnotmypassword";
